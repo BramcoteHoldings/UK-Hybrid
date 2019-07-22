f0b5fee85e48bbe1b343df9e4c29bdfd31510107
Alex <alexs@bhlinsight.com>
2019-07-22 17:27:14 +1000

    fixed issue with email recepients not appearing correctly

--- Matters.pas
+++ Matters.pas
@@ -9793,9 +9793,22 @@ begin
             else
                Msg.Subject := 'Our Ref #' + trim(qryMatter.FieldByName('fileid').AsString);
 
+            if ARecipientsList.Count > 0 then
+            begin
+               try
+                  for I := 0 to ARecipientsList.Count - 1 do
+                  begin
+                     Msg.AddRecipients(ARecipientsList.Strings[i], rtTo, True);
+                  end;
+               finally
+               end;
+            end;
+
             try
                 //FormMgr.NewMessage(Folder);
-                FormMgr.ShowMessage(Msg);
+
+               Msg.SaveChanges(smKeepOpenReadWrite);
+               FormMgr.ShowMessage(Msg);
             except on e:exception do
  	              WriteLog('EmailClick: Failed to Formmgr the message: ' + E.Message);
             end;
